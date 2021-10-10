fcc6c75f6e21cd12b05870064bee8fc6ce13119f
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-10 13:43:01 -0600

    schema

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS jtech_employees;
+CREATE DATABASE jtech_employees;
+USE jtech_employees;
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10, 2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    foreign key (department_id)
+    references department(id)
+);
+
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    department_id INTEGER NOT NULL,
+    foreign key (department_id)
+    references department(id)
+    
+    
+);
